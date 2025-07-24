a52df7fba3d7d32387cd9fec82751c511de2e36c
Divine203 <divine00329@gmail.com>
2025-07-24 10:31:35 +0100

    rotating cube

--- stages/hello-cube/hello-cube.js
+++ stages/hello-cube/hello-cube.js
@@ -1,8 +1,10 @@
-// <reference path="libs/gl-matrix.min.js" />
-
 const canvas = document.getElementById("c");
 const gl = canvas.getContext("webgl2");
 
+
+// Each face of the cube = 2 triangles
+// Each triangle = 3 vertices
+// 6 faces × 2 triangles × 3 vertices = 36 vertices
 const vertices = [
   // Front face (z = 0.5)
   -0.5, -0.5,  0.5,  // bottom-left
@@ -57,7 +59,7 @@ const vertices = [
 const getAndCompileShader = (id) => {
     let shader;
     let shaderElement = document.getElementById(id);
-    let shaderText = shaderElement.textContent.trim();
+    let shaderText = shaderElement.textContent.trim(); // the actual shader code inside the element (trim is necessary to trim spaces/lines)
 
     if(id.includes('vertex')) {
         shader = gl.createShader(gl.VERTEX_SHADER);
@@ -77,9 +79,14 @@ const getAndCompileShader = (id) => {
     return shader;
 }
 
-const positionBuffer = gl.createBuffer();
+const positionBuffer = gl.createBuffer(); // create a buffer in the GPU
 
+// B ind the buffer so all future ARRAY_BUFFER calls affect `positionBuffer`.
+// Think of bindBuffer like "selecting" this buffer to work on.
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+// Send our triangle vertex data to the GPU.
+// gl.STATIC_DRAW means: "this data won't change often"
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
 const faceColors = [
@@ -93,6 +100,7 @@ const faceColors = [
 
 let colors = [];
 
+// fill the colors array with g colors, each for every vertiex in the cube vertices array.
 faceColors.forEach((color) => {
     for (let i = 0; i < 6; i++) {
         colors = colors.concat(color);
@@ -107,75 +115,61 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 const vertexShader = getAndCompileShader("vertexShader");
 const fragmentShader = getAndCompileShader("fragmentShader");
 
-const shaderProgram = gl.createProgram();
+const shaderProgram = gl.createProgram(); // create a program
 
+// attach both shades to the program
 gl.attachShader(shaderProgram, vertexShader);
 gl.attachShader(shaderProgram, fragmentShader);
 
 gl.linkProgram(shaderProgram);
 
+// if there are any errors after linking the program
 
 if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
     console.error("Could not link shaders");
 }
 
-gl.useProgram(shaderProgram);
+gl.useProgram(shaderProgram); // use the shader program
 
-const positionAttributeLocation = gl.getAttribLocation(shaderProgram, "position"); 
-gl.enableVertexAttribArray(positionAttributeLocation); 
+// all variables created in the shader code (glsl) all have unique attributes called locations.
+// we need to get this attribute locations
+
+const positionAttributeLocation = gl.getAttribLocation(shaderProgram, "position"); // for `in vec3 position`
+gl.enableVertexAttribArray(positionAttributeLocation); // preparing the position to recieve configurations
+
+// link our gate to the vertex buffer again inorder to send the data to the position attribute in the vertex shader
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 
+
+// index: attribLocation;
+// size: how many values should be taken at once from the array data (3)
+// type: gl.FLOAT
+// stride: 0 (research better).
+// normailized: 0 means change data
+// offset: where should the count start from. (0 the begining of the array)
+gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0); // (index, size, type, normalized, stride, offset)
+
+
+// do the same for colors
 const colorAttributeLocation = gl.getAttribLocation(shaderProgram, "color");
 gl.enableVertexAttribArray(colorAttributeLocation);
 gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 gl.vertexAttribPointer(colorAttributeLocation, 4, gl.FLOAT, false, 0, 0);
 
 
-// mat4 is from the gl-matrix lib
+// Note: You can combine the position and color array data into one array and make one buffer for it. 
+// You then use the size and offset params of `vertexAttribPointer` to specify how the data should be read in the shaders
 
-const modelMatrix = mat4.create();
-const viewMatrix = mat4.create();
-const projectionMatrix = mat4.create();
-
-mat4.perspective(projectionMatrix, 45 * Math.PI/180.0, canvas.width/canvas.height, 0.1, 10); // 45deg to radians
-
-// link to them to the locations in the shaders.
-const modelMatrixLocation = gl.getUniformLocation(shaderProgram, "modelMatrix");
-const viewMatrixLocation = gl.getUniformLocation(shaderProgram, "viewMatrix");
-const projectionMatrixLocation = gl.getUniformLocation(shaderProgram, "projectionMatrix");
-
-gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
-
-let angle = 0;
 
 // Do actual drawing
 
 const runRenderLoop = () => {
     gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT); // set the color every pixel to (0, 0, 0, 1) [black]
 
-    // add depth test to sort properly
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.enable(gl.DEPTH_TEST);
-
-
-    mat4.identity(modelMatrix);
-
-    // out: the matrix where the result will be written to (your destination)
-    // a: the input matrix you want to transform (your source)
-    mat4.translate(modelMatrix, modelMatrix, [0, 0, -7]); // out, a, [x, y, z] (translation) <- this is to move the camera further from the cube
-    mat4.rotateY(modelMatrix, modelMatrix, angle); // out, a, angle
-    mat4.rotateX(modelMatrix, modelMatrix, angle/8); // out, a, angle
-
-    angle += 0.1;
-
-    //Note: Translate before you rotate.
-
-    gl.uniformMatrix4fv(modelMatrixLocation, false, modelMatrix);
-    gl.uniformMatrix4fv(viewMatrixLocation, false, viewMatrix);
-    gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
-
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
+    // draw trianle (shape, offset, amount of vertices to draw)
+    // 36 how many vertices to read
+    gl.drawArrays(gl.TRIANGLES, 0, 36); //gl.TRIANGLE tell the GL that the vertices should form a triangle
 
     requestAnimationFrame(runRenderLoop);
 }
